1d190af64ce6a6fddd0f775681d7fb8a35ebf215
liangyo8 <yonlin.liang@mail.utoronto.ca>
2020-01-16 15:30:26 -0500

    First commit

---
+++ flaskblog.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '20cce5d53f01bcbbefdac2b5e7d63873'
+
+posts = [
+    {
+        'author': 'Joseph Stalin',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Mao Zedong',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
