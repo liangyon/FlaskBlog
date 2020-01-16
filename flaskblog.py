c53ce214975b260f1addadfa254d068690763cb3
liangyo8 <yonlin.liang@mail.utoronto.ca>
2020-01-16 16:42:52 -0500

    Second commit, added login and register pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '20cce5d53f01bcbbefdac2b5e7d63873'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Joseph Stalin',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Mao Zedong',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
@@ -20,26 +20,35 @@ posts = [
 ]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
